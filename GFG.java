266dfae0bf95b3950022195095e6f61523f53604
Rakshitha-K-T <rakshithakt.xworkz596@gmail.com>
2023-11-27 10:05:40 +0530

    GFG

---
+++ GFG.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ 
+class GFG {
+
+    public static int a=10;
+	
+    public static void main(String args[])
+    {
+     System.out.println ("Hello world");  
+    int num=0;
+    }
+}
