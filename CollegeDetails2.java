2113bd8eb34bdec96b726b8420a553e3e71cda8d
Rakshitha-K-T <rakshithakt.xworkz596@gmail.com>
2023-11-27 23:39:55 +0530

    CollegeDetails2

---
+++ CollegeDetails2.java
@@ -0,0 +1,75 @@
+public class CollegeDetails2 {
+
+        public static void main(String[] args) {
+            // Student Information
+            String firstName = "Rakshitha";
+            String lastName = "K T";
+            int age = 22;
+            char gender = 'F';
+            double height = 152.3;
+            boolean isInternational = false;
+    
+            // Contact Information
+            String address = "shivanadappa badavane ";
+            String city = "nyamathi";
+            String state = "Karnataka";
+            int zipCode = 577223;
+            long phoneNumber = 9164489588L;
+            String email = "rakshithakt.xworks596@gmail.com";
+    
+            // Academic Details
+            String grade = "B E";
+            double CGPA = 7.89;
+            boolean isHonorStudent = true;
+            String favoriteSubject = "java";
+    
+            // College Information
+            String collegeName = "JNNCE";
+            String department1 = "ISE";
+            String department2 = "CSE";
+            String department3 = "ECE";
+            String department4 = "TCE";
+            String department5 = "AIML";
+            String department6 = "Civl";
+            String department7 = "mechanical";
+            String collLocation="Shimoga";
+            int collegeEstablishedYear = 1950;
+            long collegePhoneNumber = 1239874560L;
+            boolean isPublicCollege = true;
+    
+            // Financial Information
+            double scholarshipAmount = 72000;
+            boolean hasPartTimeJob = false;
+    
+            // Displaying student and college details
+            System.out.println("Student Details:");
+            System.out.println("Name: " + firstName + " " + lastName);
+            System.out.println("Age: " + age);
+            System.out.println("Gender: " + gender);
+            System.out.println("Height: " + height);
+            System.out.println("International Student: " + isInternational);
+            System.out.println("Address: " + address + ", " + city + ", " + state + " " + zipCode);
+            System.out.println("Phone Number: " + phoneNumber);
+            System.out.println("Email: " + email);
+            System.out.println("Grade: " + grade);
+            System.out.println("CGPA: " + CGPA);
+            System.out.println("Honor Student: " + isHonorStudent);
+            System.out.println("Favorite Subject: " + favoriteSubject);
+    
+            System.out.println("\nCollege Details:");
+            System.out.println("College Name: " + collegeName);
+            System.out.println("Department: " + department1);
+            System.out.println("Department: " + department2);
+            System.out.println("Department: " + department3);
+            System.out.println("Department: " + department4);
+            System.out.println("Department: " + department5);
+            System.out.println("Department: " + department6);
+            System.out.println("Department: " + department7);
+            System.out.println("Established Year: " + collegeEstablishedYear);
+            System.out.println("College Phone Number: " + collegePhoneNumber);
+            System.out.println("Public College: " + isPublicCollege);
+            System.out.println("Scholarship Amount: " + scholarshipAmount);
+            System.out.println("Part-Time Job: " + hasPartTimeJob);
+        }
+    
+}
